96455b0f185adeb711ed940fda60a7d9230da88d
coldrain-f <intmain__@naver.com>
2022-09-08 18:40:56 +0900

    feat: users resource 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import * as Joi from 'joi';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     DatabaseModule,
     AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
