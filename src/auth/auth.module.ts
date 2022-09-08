9fdcce9fc2195ec71cc2bf4d3e4fd38f6b35b1dc
coldrain-f <intmain__@naver.com>
2022-09-08 18:37:26 +0900

    feat: auth module, guard, strategy 설정 및 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import { JwtStrtegy } from './jwt/jwt.strategy';
 
 @Module({
-  providers: [AuthService],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt', session: false }),
+    JwtModule.register({
+      secret: 'secret',
+      signOptions: { expiresIn: '1y' },
+    }),
+  ],
+  providers: [AuthService, JwtStrtegy],
 })
 export class AuthModule {}
---
+++ src/auth/jwt/jwt.strategy.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrtegy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'secretKey',
+    });
+  }
+
+  //async validate(payload) {}
+}
