dcfac958b28d5fc020574fd1ceb1b5abf8fc2db1
coldrain-f <intmain__@naver.com>
2022-09-08 14:48:49 +0900

    feat: Swagger 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+import { INestApplication, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('SERVER_PORT');
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('title')
+    .setDescription('description')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(PORT);
+  Logger.log(`Application running on port ${PORT}, http://localhost:${PORT}`);
+  Logger.log(`Go to API Docs : http://localhost:${PORT}/docs`);
 }
+
 bootstrap();
