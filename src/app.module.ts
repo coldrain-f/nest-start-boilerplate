58d50155b62425d8c8d1384b7151c6e26a9d50ea
coldrain-f <intmain__@naver.com>
2022-09-08 15:52:09 +0900

    feat: TypeORM 설정 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import * as Joi from 'joi';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       validationSchema: Joi.object({
         SERVER_PORT: Joi.number().default(3000),
+        SERVER_MODE: Joi.string().default('dev'),
         DATABASE_HOST: Joi.string().required(),
         DATABASE_PORT: Joi.number().required(),
         DATABASE_USERNAME: Joi.string().required(),
@@ -16,6 +18,7 @@ import * as Joi from 'joi';
         DATABASE_NAME: Joi.string().required(),
       }),
     }),
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
