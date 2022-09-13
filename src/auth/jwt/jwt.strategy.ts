e1e61ffa5c1b9a88966d386843d35d0e1c30b135
coldrain-f <intmain__@naver.com>
2022-09-13 14:36:57 +0900

    feat: jwt.strategy에 validate 함수 구현

--- src/auth/jwt/jwt.strategy.ts
+++ src/auth/jwt/jwt.strategy.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
+import { Payload } from './jwt.payload';
 
 @Injectable()
 export class JwtStrtegy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private readonly usersRepository: Repository<User>) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -12,5 +15,17 @@ export class JwtStrtegy extends PassportStrategy(Strategy) {
     });
   }
 
-  //async validate(payload) {}
+  async validate(payload: Payload) {
+    const user = await this.usersRepository.findOne({
+      select: { password: false },
+      where: { id: parseInt(payload.sub) },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    // Controller에서 @Req를 주입받으면 여기서 반환한 user를 사용할 수 있다.
+    return user;
+  }
 }
